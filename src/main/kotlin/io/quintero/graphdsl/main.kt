9cff9b551be95e68fc8da650d99e6942b1facf29
Mathias Quintero <me@quintero.io>
2019-12-30 05:55:53 +0100

    using star wars as default

--- src/main/kotlin/io/quintero/graphdsl/main.kt
+++ src/main/kotlin/io/quintero/graphdsl/main.kt
@@ -4,8 +4,10 @@ import io.quintero.graphdsl.codegen.builder.KotlinCodeBuilder
 import io.quintero.graphdsl.schema.Schema
 import io.quintero.graphdsl.schema.fetch
 
+private const val starWarsAPI = "https://swapi-graphql.netlify.com/.netlify/functions/index"
+
 fun main(args: Array<String>) {
-    val url = args.firstOrNull() ?: "https://countries.trevorblades.com/"
+    val url = args.firstOrNull() ?: starWarsAPI
     val schema = Schema.fetch(url)
     print(KotlinCodeBuilder().apply { +schema }.build())
 }
